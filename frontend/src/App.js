c340cf6918c24a559432fa842b5e70f274eb7d0b
sanjeet kumar <skc0845@gmail.com>
2023-12-20 01:06:49 +0530

    added:header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
+import Header from "../src/components/Header/Header";
 function App() {
   return (
     <div className="App">
-      <h1>music art</h1>
+      <Header />
     </div>
   );
 }
---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,33 @@
+import style from "./Header.module.css";
+import phoneIcon from "../../assets/phoneIcon.svg";
+import musicIcon from "../../assets/musicIcon.svg";
+import { useState } from "react";
+const Header = () => {
+  const [login, setLogin] = useState(false);
+  return (
+    <header className={style.desktopHeader}>
+      <div className={style.leftBox}>
+        <img src={phoneIcon} alt="phoneIcon" />
+        <span>912121131313</span>
+      </div>
+      <div className={style.middleBox}>
+        <span>Get 50% off on selected items | Shop Now</span>
+      </div>
+      <div className={style.rightBox}>
+        {login ? (
+          <button>Logout</button>
+        ) : (
+          <>
+            <a href="#login">Login</a> | <a href="#signup">Signup</a>
+          </>
+        )}
+      </div>
+      <div className={style.mobileHeader}>
+        <img src={musicIcon} alt="musicIcon" />
+        <span>Musicart</span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;

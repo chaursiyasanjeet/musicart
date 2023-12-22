4ab4ea9fc5f6eb9ba49bbdc0b16da5e1effb6dff
sanjeet kumar <skc0845@gmail.com>
2023-12-23 04:23:23 +0530

    added:order success page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,18 @@ import Signup from "./pages/Signup/Signup";
 import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
 import ProductDetails from "./pages/ProductDetails/ProductDetails";
+import OrderSuccess from "./pages/OrderSuccess/OrderSuccess";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/n" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Login />} />
-          <Route path="/" element={<ProductDetails />} />
+          <Route path="/productid" element={<ProductDetails />} />
+          <Route path="/ordersuccess" element={<OrderSuccess />} />
         </Routes>
       </BrowserRouter>
     </div>

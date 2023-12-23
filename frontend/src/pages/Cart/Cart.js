0a94b2239cbaeb71d6ccc3e7a455265ac21ff032
sanjeet kumar <skc0845@gmail.com>
2023-12-24 03:54:02 +0530

    add:cartMobile

--- frontend/src/pages/Cart/Cart.js
+++ frontend/src/pages/Cart/Cart.js
@@ -1,9 +1,11 @@
 import CartDesktop from "./Desktop/CartDesktop";
+import CartMobile from "./Mobile/CartMobile";
 
 const Cart = () => {
   return (
     <>
       <CartDesktop />
+      <CartMobile />
     </>
   );
 };
---
+++ frontend/src/pages/Cart/Mobile/CartMobile.js
@@ -0,0 +1,78 @@
+import style from "./CartMobile.module.css";
+import MobileSearchBar from "../../../components/MobileSearchBar/MobileSearchBar";
+import MobileNavFooter from "../../../components/MobileNavFooter/MobileNavFooter";
+import { useNavigate } from "react-router-dom";
+import backIcon from "../../../assets/backIcon.svg";
+import headphone from "../../../assets/headphone.png";
+
+const CartMobile = () => {
+  const redirect = useNavigate();
+  return (
+    <>
+      <MobileSearchBar />
+      <div className={style.container}>
+        <div className={style.backButton}>
+          <img
+            src={backIcon}
+            alt="backArrow"
+            onClick={() => {
+              redirect("/");
+            }}
+          />
+        </div>
+        <div className={style.allCartProduct}>
+          <div>
+            <img src={headphone} alt="headphoneimg" />
+            <div className={style.productDetails}>
+              <span>Sony WH-CH720N</span>
+              <span>₹3500</span>
+              <span>Clour : Black</span>
+              <span>In Stock</span>
+              <span>
+                Convenience Fee <span>₹45</span>
+              </span>
+            </div>
+          </div>
+          <div>
+            <img src={headphone} alt="headphoneimg" />
+            <div className={style.productDetails}>
+              <span>Sony WH-CH720N</span>
+              <span>₹3500</span>
+              <span>Clour : Black</span>
+              <span>In Stock</span>
+              <span>
+                Convenience Fee <span>₹45</span>
+              </span>
+            </div>
+          </div>
+          <div>
+            <img src={headphone} alt="headphoneimg" />
+            <div className={style.productDetails}>
+              <span>Sony WH-CH720N</span>
+              <span>₹3500</span>
+              <span>Clour : Black</span>
+              <span>In Stock</span>
+              <span>
+                Convenience Fee <span>₹45</span>
+              </span>
+            </div>
+          </div>
+          <summary>
+            <span>Total:</span>
+            <span>₹3545</span>
+          </summary>
+        </div>
+        <div className={style.totalAmountAndOrderBtn}>
+          <div>
+            <span>Total Amount</span>
+            <span>₹3500</span>
+          </div>
+          <button>PLACE ORDER</button>
+        </div>
+      </div>
+      <MobileNavFooter component={"cart"} />
+    </>
+  );
+};
+
+export default CartMobile;
